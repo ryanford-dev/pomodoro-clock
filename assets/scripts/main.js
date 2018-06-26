3213417250b51f3370331c37bd8238b49432bd21
Ryan Ford <ryanford@email.com>
2018-06-26 20:00:31 +0800

    Update some es6+ goodness

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,100 +1,99 @@
 'use strict';
 
-String.prototype.toSeconds = function() {
-	let time = this.split(":"),
-		hours = +time[0],
-		minutes = +time[1],
-		seconds = +time[2];
+const toSeconds = n => {
+	const time = n.split(':');
+	const hours = Number(time[0]);
+	const minutes = Number(time[1]);
+	const seconds = Number(time[2]);
 
-		return hours * 60 * 60 + minutes * 60 + seconds;
+	return hours * 60 * 60 + minutes * 60 + seconds;
 }
 
-Number.prototype.toTime = function() {
+const toTime = n => {
 	let date = new Date(null);
-	date.setSeconds(this);
+	date.setSeconds(n);
 	return date.toISOString().substr(11,8);
 }
 
-var running = false,
-	mode = "work-time",
-	start = 0,
-	chime = new Audio("assets/sound/notification.wav");
+let running = false;
+let	mode = 'work-time';
+let	start = 0;
+const	chime = new Audio('assets/sound/notification.wav');
 
-function incrementTimer(e) {
-	e.preventDefault();
+const incrementTimer = e => {
 	if (running) return;
-	let button = (e.target.tagName !== "button") ? e.target.parentElement : e.target, 
-		timer = button.parentNode.querySelector("span"),
-		time = timer.textContent.toSeconds(),
-		control = button.getAttribute('data-controls'),
-		currentTime = document.querySelector("h1");
+	const button = e.target;
+	const	timer = button.parentNode.querySelector('span');
+	const	control = button.dataset.controls;
+	const	currentTime = document.querySelector('h1');
+	let	time = toSeconds(timer.textContent);
 
-	if (control === "less") {
+	if (control === 'less') {
 		if (time > 60) {
 			time -= 60;
 		} else {
-			timer.classList.add("error");
-			window.setTimeout(function(){
-				timer.removeAttribute("class");
+			timer.classList.add('error');
+			window.setTimeout(() => {
+				timer.removeAttribute('class');
 			}, 100);
 			return;
 		}
 	} else {
 		time += 60;
 	}
-	timer.textContent = time.toTime();
-	if (button.parentElement.parentElement.classList.contains("work-time")) {
-	currentTime.textContent = time.toTime();
+	timer.textContent = toTime(time);
+	if (button.parentElement.parentElement.classList.contains('work-time')) {
+	currentTime.textContent = toTime(time);
 	}
 }
 
-function update() {
+const update = () => {
 	if (running) {
-		let currentTime = document.querySelector("h1").textContent,
-			time = currentTime.toSeconds(),
-			modeIndicator = document.querySelector("." + mode + " h3"),
-			active = document.querySelector(".active"),
-			timer = document.querySelector("." + mode + " .controls span").textContent.toSeconds();
+		const currentTime = document.querySelector('h1').textContent;
+		const	modeIndicator = document.querySelector('.' + mode + ' h3');
+		const	active = document.querySelector('.active');
+		let	timer = toSeconds(document.querySelector('.' + mode + ' .controls span').textContent);
+		let time = toSeconds(currentTime);
 
 		if (!active || active.textContent.toLowerCase() !== mode) {
 			if (active) {
-				active.removeAttribute("class");
+				active.removeAttribute('class');
 			}
-			modeIndicator.classList.add("active");
+			modeIndicator.classList.add('active');
 		}
 
 		if (time <= 0) {
-			mode = (mode === "work-time") ? "break-time" : "work-time";
-			document.querySelector("h1").textContent = document.querySelector("." + mode + " .controls span").textContent;
+			mode = (mode === 'work-time') ? 'break-time' : 'work-time';
+			document.querySelector('h1').textContent = document.querySelector('.' + mode + ' .controls span').textContent;
 			start = performance.now() / 1000;
 			chime.play();
 			return update();
 		}
 
 		timer = (start - performance.now() / 1000) + timer;
-		document.querySelector("h1").textContent = timer.toTime();
+		document.querySelector('h1').textContent = toTime(timer);
 
 		window.requestAnimationFrame(update);
 	}
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-	var buttons = document.querySelectorAll(".settings .controls button");
-	for (let i = 0, len = buttons.length; i < len; i++) {
-		buttons[i].addEventListener("click", incrementTimer);
-	}
-	document.getElementById("switch").addEventListener("click", function(e) {
+document.addEventListener('DOMContentLoaded', function() {
+	const buttons = document.querySelectorAll('.settings .controls button');
+	buttons.forEach(button => {
+		button.addEventListener('click', incrementTimer);
+	})
+	document.getElementById('switch').addEventListener('click', function(e) {
 		if (!running) {
 			start = performance.now() / 1000;
 			running = true;
 			update();
-			e.target.textContent = "stop";
+			e.target.firstChild.textContent = 'stop';
 		} else {
 			running = false;
-			mode = "work-time";
-			document.querySelector(".active").removeAttribute("class");
-			document.querySelector("h1").textContent = document.querySelector(".work-time .controls span").textContent;
-			e.target.textContent = "play_arrow"
+			mode = 'work-time';
+			document.querySelector('.active').removeAttribute('class');
+			document.querySelector('h1').textContent = document.querySelector('.work-time .controls span').textContent;
+			e.target.firstChild.textContent = 'play_arrow'
 		}
 	});
 });
